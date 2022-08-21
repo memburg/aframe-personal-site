30aeff86677729a9ec142ba0a44b0a6087b0a51d
Ramb Memburg <heisramby@gmail.com>
2022-08-21 18:33:59 -0500

    crystalize logic

---
+++ Source/Scripts/Crystal.js
@@ -0,0 +1,22 @@
+AFRAME.registerComponent('crystalize', {
+    schema: {
+        opacity: { type: "number", default: 0.50 }
+    },
+    init: function () {
+        this.el.addEventListener("model-loaded", this.update.bind(this));
+    },
+    update: function () {
+        let mesh = this.el.getObject3D("mesh");
+        let data = this.data;
+        if (!mesh) {
+            return;
+        }
+        mesh.traverse(function (node) {
+            if (node.isMesh) {
+                node.material.opacity = data.opacity;
+                node.material.transparent = data.opacity < 1.0;
+                node.material.needsUpdate = true;
+            }
+        });
+    }
+});
